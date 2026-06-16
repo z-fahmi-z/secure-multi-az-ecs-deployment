8a0d273392b493eb9ca8441ea42eb01f73086c78
z-fahmi-z <fahmi.zaid1412@gmail.com>
2026-06-16 19:32:52 +0200

    fix: allow image tag update in ECR

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -129,7 +129,8 @@ resource "aws_cloudtrail" "this" {
 # ECR repository 
 resource "aws_ecr_repository" "app" {
   name                 = var.ecr_repository_name
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
