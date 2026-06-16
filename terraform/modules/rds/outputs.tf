ad3789d87eb00f7a47f134c3ab5a70e5ad0da3d7
z-fahmi-z <fahmi.zaid1412@gmail.com>
2026-06-16 19:35:18 +0200

    fix: remove redundant output from RDS module

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -8,11 +8,6 @@ output "db_main_port" {
   value       = aws_db_instance.main.port
 }
 
-output "db_main_endpoint" {
-  description = "The connection endpoint for the main RDS instance (host:port)"
-  value       = aws_db_instance.main.endpoint
-}
-
 output "db_master_secret_arn" {
   value = aws_db_instance.main.master_user_secret[0].secret_arn
 }
